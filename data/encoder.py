abdc3b0ed61d1f00c8fa9689845f11c66660911d
Justinas Zaliaduonis <justinaszaliaduonis@Justinass-MacBook-Pro.local>
2025-02-19 00:46:47 +0200

    Add encoder from CLIDGP

--- data/encoder.py
+++ data/encoder.py
@@ -2,20 +2,27 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class SphereEncoder(nn.Module):
-    def __init__(self, input_dim, latent_dim):
-        super(SphereEncoder, self).__init__()
-        # self.encoder = nn.Sequential(
-        #     nn.Linear(input_dim, 256),  # First layer
-        #     nn.ReLU(),                 # Non-linear activation
-        #     nn.Linear(256, 128),       # Second layer
-        #     nn.Sigmoid(),
-        #     nn.Linear(128, latent_dim) # Final layer to map to latent_dim
-        # )
 
-        self.encoder = nn.Linear(input_dim, latent_dim)
+# Define the encoder model
+class SphereEncoder(nn.Module):
+    def __init__(self):
+        super(SphereEncoder, self).__init__()
+        self.f = nn.Sequential(
+            nn.Linear(3, 30),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Linear(30, 150),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Linear(150, 150),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Linear(150, 150),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Linear(150, 150),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Linear(150, 30),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Linear(30, 3)
+        )
 
     def forward(self, x):
-        z = self.encoder(x)
+        return F.normalize(self.f(x), dim=1, p=2)
 
-        return F.normalize(z, dim=1, p=2)
